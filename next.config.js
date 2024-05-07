0bcd35c02ade40c5f2aac7466146b320c8df3bb5
Max Strootmann <max@nngrafischontwerp.nl>
2024-05-07 09:02:40 +0200

    replace : by = in .env files

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./src/env.ts");
 
 /** @type {import("next").NextConfig} */
 const config = {};
--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -1,6 +1,5 @@
 "use server";
 import { Resend } from "resend";
-
 import { env } from "~/env";
 
 const resend = new Resend(env.RESEND_API_KEY);
