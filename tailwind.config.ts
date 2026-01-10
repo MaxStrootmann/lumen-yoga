f5ea2cdee9b1d7ceefa65a9a8a5c543da1d3c0e6
MaxStrootmann <max@manndigital.nl>
2026-01-10 11:06:39 +0100

    fix deploy tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
-  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
